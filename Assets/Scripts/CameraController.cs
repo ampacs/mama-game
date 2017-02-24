ae83a6be5d5fbe93a4c593bdce3148f495841005
Elianabeth <elianabethcohen@gmail.com>
2017-02-24 15:48:27 -0500

    Updated Camera movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
-
+public class CameraController: MonoBehaviour {
+    // Here's a camera implementation based on boundry movement
+    // Doesn't have to be used
+    /*
     public float smoothTime = 0.05f,
                  cameraOffsetPositionVelocityMultiplier = 0.3333f;
     public Vector3 cameraVelocityMultiplier = new Vector3(20f, 30f, 10f);
@@ -31,4 +33,153 @@ public class CameraController : MonoBehaviour {
         currentVelocity *= Time.deltaTime;
         transform.Translate(currentVelocity);
 	}
+    */
+
+
+    // create new collision detector
+    public CollisionController target;
+    //use vector to represent size of focus area
+    public Vector2 focusAreaSize;
+    // hover camera over player
+    public float verticalOffset;
+    // different camera smoothing/looking ahead variables
+    public float lookAheadDstX;
+    public float lookSmoothTimeX;
+    public float verticalSmoothTime;
+    // create a new focusArea struct
+    FocusArea focusArea;
+
+    // more camera smoothing variables/ look ahead vars
+    float currentLookAheadX;
+    float targetLookAheadX;
+    float lookAheadDirX;
+    float smoothLookVelocityX;
+    float smoothVelocityY;
+
+    bool lookAheadStopped;
+
+
+    void Start()
+    {
+        // init focusArea
+        focusArea = new FocusArea(target.boxCollider.bounds, focusAreaSize);
+    }
+    // update after player moves
+    void LateUpdate()
+    {
+        focusArea.Update(target.boxCollider.bounds);
+
+        //Put camera at focusPosition
+        Vector2 focusPosition = focusArea.center + Vector2.up * verticalOffset;
+        
+        // If camera's x is moving, change dirX
+        if (focusArea.velocity.x != 0)
+        {
+            lookAheadDirX = Mathf.Sign(focusArea.velocity.x);
+            // If moving in same direction
+            if (Mathf.Sign(target.dirX) == Mathf.Sign(focusArea.velocity.x) && target.dirX != 0)
+            {
+                lookAheadStopped = false;
+                targetLookAheadX = lookAheadDirX * lookAheadDstX;
+            }
+            else
+            {
+                if (!lookAheadStopped)
+                {
+                    //compensation for opposing directions
+                    lookAheadStopped = true;
+                    targetLookAheadX = (currentLookAheadX + (lookAheadDirX * lookAheadDstX - currentLookAheadX)) / 4;
+                }
+            }
+        }
+
+        //smooth Y
+        focusPosition.y = Mathf.SmoothDamp(transform.position.y, focusPosition.y, ref smoothVelocityY, verticalSmoothTime);
+
+        //change look ahead depending on distance and dir
+        targetLookAheadX = lookAheadDirX * lookAheadDstX;
+        //smooth change
+        currentLookAheadX = Mathf.SmoothDamp(currentLookAheadX, targetLookAheadX, ref smoothLookVelocityX, lookSmoothTimeX);
+
+        focusPosition += Vector2.right * currentLookAheadX;
+        //make sure camera is in front
+        transform.position = (Vector3)focusPosition + Vector3.forward * -10;
+    }
+    // View collision box
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, .5f);
+        Gizmos.DrawCube(focusArea.center, focusAreaSize);
+    }
+    //define FocusArea
+    struct FocusArea
+    {
+        public Vector2 center;
+        float left, right;
+        float top, bottom;
+        public Vector2 velocity;
+
+        public FocusArea(Bounds targetBounds, Vector2 size)
+        {
+            //define borders, and center
+            left = targetBounds.center.x - size.x / 2;
+            right = targetBounds.center.x + size.x / 2;
+            bottom = targetBounds.min.y;
+            top = targetBounds.min.y + size.y;
+            velocity = Vector2.zero;
+            center = new Vector2((left + right) / 2, (top + bottom) / 2);
+        }
+
+        public void Update(Bounds targetBounds)
+        {
+            //determines amount camera should move if on edge
+            float shiftX = 0;
+
+            //If target moves more left past collision box
+            if (targetBounds.min.x < left)
+            {
+                //move it over by difference between two
+                shiftX = targetBounds.min.x - left;
+            }
+
+            //if target moves more right collision box
+            else if (targetBounds.max.x > right)
+            {
+                //move it over by difference between two
+                shiftX = targetBounds.max.x - right;
+            }
+
+            //move whole collision box by difference
+            left += shiftX;
+            right += shiftX;
+
+            // Now find differences in Y
+            float shiftY = 0;
+
+            //If target moves lower past collision box
+            if (targetBounds.min.y < bottom)
+            {
+                //move it over by difference between two
+                shiftY = targetBounds.min.y - bottom;
+            }
+
+            //if target moves higher above collision box
+            else if (targetBounds.max.y > top)
+            {
+                //move it over by difference between two
+                shiftY = targetBounds.max.y - top;
+            }
+
+            //move whole collision box by difference
+            top += shiftY;
+            bottom += shiftY;
+            // calculate new center
+            center = new Vector2((left + right) / 2, (top + bottom) / 2);
+            velocity = new Vector2(shiftX, shiftY);
+
+
+        }
+    }
+
+
 }
--- Assets/Scripts/CollisionControllerEB.cs
+++ Assets/Scripts/CollisionControllerEB.cs
@@ -1,10 +1,200 @@
+//<<<<<<< HEAD:Assets/Scripts/Controller2D.cs
+/*
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Requires box collider
+[RequireComponent(typeof(BoxCollider2D))]
+public class Controller2D : MonoBehaviour {
+    public LayerMask collisionMask;
+    //Allows you to change the rays projected
+    public int horizontalRayCount = 4;
+    public int verticalRayCount = 4;
+    //Variables for calculating spacing for rays
+    float horizontalRaySpacing;
+    float verticalRaySpacing;
+    //Current width of skin
+    const float skinWidth = .015f;
+    // Create a collider
+    public BoxCollider2D colliderControl;
+    RaycastOrigins raycastOrigins;
+    public CollisionInfo collisions;
+
+	void Awake () {
+        //init collider from current controller
+        colliderControl = this.GetComponent<BoxCollider2D>();
+        //determine spacing for ray casting
+        calculateRaySpacing();
+    }
+
+    public void Start()
+    {
+
+    }
+    void Update()
+    {
+
+
+    }
+
+    //Movement functions
+    public void Move(Vector3 velocity)
+    {
+        collisions.Reset();
+        //update raycast positions before moving
+        updateRaycastOrigins();
+        //Only check if moving in direction
+        if (velocity.x != 0) { 
+            //Check Horizontal Collisions
+            HorizontalCollisions(ref velocity);
+        }
+        if (velocity.y != 0)
+        {
+            //Check for collisions on velocity
+            VerticalCollisions(ref velocity);
+        }
+        //Translate the object according to velocity
+        transform.Translate(velocity);
+    }
+
+    //Handles vertical collisions
+    void VerticalCollisions(ref Vector3 velocity)
+    {
+        //Direction of velocity
+        float directionY = Mathf.Sign(velocity.y);
+        //Length of ray (plus skin), since inset in skin
+        float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+        //Draw all of the vertical rays in debug mode
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            //If you're moving down, start with bottom left, else topleft
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            //Sets offsets for each ray
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+            //Detect a hit from each ray cast
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+            //Draw ray in debug mode
+            Debug.DrawRay(rayOrigin,  Vector2.up * directionY * rayLength, Color.red);
+            //act according to collision if hit
+            if (hit)
+            {
+                //Bounce off, making sure to count for ignored skin
+                velocity.y = (hit.distance - skinWidth) * directionY;
+                //Set to hit distance, to prevent clipping on edges
+                rayLength = hit.distance;
+
+                if (directionY == -1)
+                {
+                    collisions.below = true;
+                    collisions.above = false;
+                }
+                else if (directionY == 1)
+                {
+                    collisions.above = true;
+                    collisions.below = false;
+                }
+            }
+        }
+
+    }
+
+
+    //Handles horizontal collisions
+    void HorizontalCollisions(ref Vector3 velocity)
+    {
+        //Direction of velocity
+        float directionX = Mathf.Sign(velocity.x);
+        //Length of ray (plus skin), since inset in skin
+        float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+        //Draw all of the vertical rays in debug mode
+        for (int i = 0; i < horizontalRayCount; i++)
+        {
+            //If you're moving down, start with bottom left, else bottom right
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+            //Sets offsets for each ray
+            rayOrigin += Vector2.up * horizontalRaySpacing * i;
+            //Detect a hit from each ray cast
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+            //Draw ray in debug mode
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+            //act according to collision if hit
+            if (hit)
+            {
+                //Bounce off, making sure to count for ignored skin
+                velocity.x = (hit.distance - skinWidth) * directionX;
+                //Set to hit distance, to prevent clipping on edges
+                rayLength = hit.distance;
+                //If you were going in X direction, you collided with X side
+                if (directionX == -1)
+                {
+                    collisions.left = true;
+                    collisions.right = false;
+                }
+                else if (directionX == 1)
+                {
+                    collisions.right = true;
+                    collisions.left = false;
+                }
+            }
+        }
+
+    }
+
+    //Ray update functions
+    void calculateRaySpacing()
+    {
+        //Get bounds, and shrink the skin
+        Bounds bounds = colliderControl.bounds;
+        bounds.Expand(skinWidth * -2);
+        //Make a restriction so that raycounts are between 2 and max
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+        //Calculate even spacing
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+    void updateRaycastOrigins()
+    {
+        //Get bounds of collider
+        Bounds bounds = colliderControl.bounds;
+        //shrink the skin by 0.2
+        bounds.Expand(skinWidth * -2);
+
+        //Define origins for rays
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+    }
+	
+    //Define struct for different origins
+    struct RaycastOrigins
+    {
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+
+    }
+    public struct CollisionInfo
+    {
+        public bool above, below;
+        public bool left, right;
+
+        public void Reset()
+        {
+            above = below = false;
+            left = right = false;
+        }
+    }
+}
+*/
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 //Requires box collider
 [RequireComponent(typeof(BoxCollider2D))]
-public class CollisionController : MonoBehaviour {
+public class CollisionControllerEB : MonoBehaviour {
 
     public LayerMask collisionMask;
 
@@ -16,12 +206,12 @@ public class CollisionController : MonoBehaviour {
     float horizontalRaySpacing;
     float verticalRaySpacing;
 
-    //Current width of skin
-    const float skinWidth = .015f;
-
-    //directional values
+    //keep track of current movement
     public float dirX;
     public float dirY;
+
+    //Current width of skin
+    const float skinWidth = .015f;
 
     // Create a collider
     public BoxCollider2D boxCollider;
@@ -32,7 +222,6 @@ public class CollisionController : MonoBehaviour {
     {
 
     }
-
 
 	void Awake () {
         //init collider from current controller
@@ -226,3 +415,4 @@ public class CollisionController : MonoBehaviour {
         }
     }
 }
+//>>>>>>> refs/remotes/origin/master:Assets/Scripts/CollisionController.cs
