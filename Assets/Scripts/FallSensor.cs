4e6829ecb328fd5703b15421f32b91eafe39f628
Untellable <benklingensmith@gmail.com>
2017-03-21 23:25:53 -0400

    Update on Newborn stage and progress on toddler

---
+++ Assets/Scripts/FallSensor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallSensor : MonoBehaviour {
+    public AudioSource noise;
+    private bool NoiseMade;
+	// Use this for initialization
+	void Start () {
+        NoiseMade = false;
+	}
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!NoiseMade && other.collider.CompareTag("Player"))
+        {
+            noise.Play();
+            NoiseMade = true;
+        }
+    }
+}
