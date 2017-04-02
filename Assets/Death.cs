c0541b068c1b9c707fa4b2fe9d5fa249ae06c22a
mbofenka <mbofenka@andrew.cmu.edu>
2017-04-02 13:45:40 -0400

    Checkpoint Prefab Created
    In order to restart the level in the correct way, simply call the line
    of code that I’ve marked in the Death script.

---
+++ Assets/Death.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (transform.position.y < -20.0f)
+			GameObject.FindGameObjectWithTag ("Checkpoint").GetComponent<Checkpoint> ().RestartLevel ();
+		
+	}
+}
